4fd3d444b616ccc7fd2a32b986ff5ae756db18c2
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2022-01-03 11:19:05 +0530

    fix default delay

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   session: JSON.parse(process.env.SESSION),
   chat_id: process.env.CHAT_ID,
   category: process.env.CATEGORY || "all",
-  delay: parseInt(process.env.DELAY) || 150000
+  delay: parseInt(process.env.DELAY) || 150
 };
